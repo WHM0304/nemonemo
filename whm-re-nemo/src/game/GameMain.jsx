f2e70f9b5fc57ec3f3f74205222a82511e80ea43
WHM0304 <whm0304@naver.com>
2024-05-13 13:47:37 +0900

    react

--- whm-re-nemo/src/game/GameMain.jsx
+++ whm-re-nemo/src/game/GameMain.jsx
@@ -2,16 +2,43 @@ import { useEffect, useState } from "react";
 import "../css/Game.css";
 
 const GameMain = () => {
-  const [values, SetValues] = useState([5][5]);
+  const [values, SetValues] = useState(() => {
+    const rows = 5;
+    const columns = 5;
+    return Array.from({ length: rows }, () => Array(columns).fill(0));
+  });
+  const updateState = (rowIndex, columnIndex, value) => {
+    SetValues((prevState) => {
+      const newState = [...prevState];
+      newState[rowIndex][columnIndex] = value;
+      return newState;
+    });
+  };
+
   const [part, SetPart] = useState("");
+  let play = [];
+  for (let i = 0; i < 5; i++) {
+    let _play = [];
+    for (let j = 0; j < 5; j++) {
+      _play.push(0);
+    }
+    play.push(_play);
+  }
+
   const onClickValue = (e) => {
     const target = e.target;
-    target.checked ? (target.value = "1") : (target.value = "0");
+    target.checked ? (target.value = 1) : (target.value = 0);
     SetPart(target.value);
     const row = target.name.substr(7) - 1;
-    // console.log(target.closest("div").name);
-    console.log(row);
-    console.log(target.value);
+    const column = target.parentElement.className.substr(9) - 1;
+    play[column][row] = part;
+    // SetValues[column][row] = target.value;
+    // console.log(column, row);
+    // console.log(play);
+    // console.log(part);
+    // console.log(values);
+    // console.log(row);
+    // console.log(target.value);
 
     // console.log(values);
     // SetValues([[...part, ...part], target.value]);
